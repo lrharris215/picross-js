2c111118690811d61f0fbfdeae9618c089dcd6ec
Lane Harris <lrharris215@gmail.com>
2021-04-05 13:24:54 -0400

    added handle click functions to square

--- public/javascripts/square.js
+++ public/javascripts/square.js
@@ -3,7 +3,29 @@ class Square {
         this.status = "unclicked"; // unclicked, filled, exed, maybe
     }
 
-   
+    handleClick() {
+        if (this.status=== "filled") {
+            this.status = 'unclicked';
+        }else {
+            this.status === "filled"
+        }
+    }
+
+    handleRightClick() {
+        if (this.status === "exed") {
+            this.status = "unclicked";
+        }else {
+            this.status = "exed"
+        }
+    }
+
+    handleMiddleClick() {
+        if (this.status === "maybe") {
+            this.status = "unclicked"
+        }else {
+            this.status = "maybe"
+        }
+    }
 }
 
 
