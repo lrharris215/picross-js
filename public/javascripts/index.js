f985168e1dda46394438ac2bc6cc8a300b6e2c78
Lane Harris <lrharris215@gmail.com>
2021-04-05 17:55:44 -0400

    started styling number boxes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,7 +5,7 @@ import '../styles/index.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
     let topNums = [[5],[2],[4], [1,1,1],[2]]
-    let leftNums = [[5],[3,1], [1,2], [1,1], [1]]
+    let leftNums = [[5],[3,1], [1,2], [1,1], [1,1]]
 let board = new Board(5, topNums, leftNums);
 board.render();
 })
