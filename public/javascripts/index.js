c1a1b8bb77b4c8cefb2c9bee786eed21b5b83829
Lane Harris <lrharris215@gmail.com>
2021-04-06 15:56:59 -0400

    added instruction button functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,22 @@ import Board from './board';
 import Level from './level';
 import '../styles/index.scss';
 import Game from './game';
+
 document.addEventListener('DOMContentLoaded', () => {
+    const instructionButton = document.getElementById('instructions-button');
+    const instructionDetail = document.getElementById('instructions-detail');
+
+    const toggleInstructions = () => {
+        if (instructionDetail.className === 'active') {
+            instructionDetail.className = 'hidden';
+        } else {
+            instructionDetail.className = 'active';
+        }
+    };
+    instructionButton.addEventListener('click', () => {
+        toggleInstructions();
+    });
+
     let level = new Level(5, '1111111101101101010010010');
     // let row = level.rowVals();
     // let col = level.colVals();
